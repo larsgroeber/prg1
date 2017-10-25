4688daa71a622e3f10533d3d3561a3022b93f682
Lars Gröber <larsgroeber7@gmail.com>
2017-10-25 17:46:28 +0200

    finished prg sheet 1

--- Tools/pack.py
+++ Tools/pack.py
@@ -1,2 +1,21 @@
+__author__ = ""
 #!/usr/bin/env python
+import glob
+import sys
 
+import os
+
+
+def add_author_variable(filename):
+    with open(filename, 'r+') as f:
+        content = f.read()
+        if len(content) > 0 and "__author__" not in content[:10]:
+            f.seek(0, 0)
+            f.write("__author__\n" + content)
+
+
+directory = sys.argv[1]
+
+python_files = glob.glob(directory + "/*.py")
+for file in python_files:
+    add_author_variable(file)
