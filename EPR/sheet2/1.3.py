fdfd96bd59cf49303196915cca9d9171a1015505
Lars Gröber <larsgroeber7@gmail.com>
2017-11-08 23:40:56 +0100

    did 3.1 and 3.2 prg

---
+++ EPR/sheet2/1.3.py
@@ -0,0 +1,18 @@
+import datetime
+
+date_string = input("Please enter a date in the format 'DDMMYYYY': ")
+
+day = int(date_string[:2])
+month = int(date_string[2:4])
+year = int(date_string[4:])
+
+date = datetime.date(year, month, day)
+
+christmas_year = year
+if month == 12 and day > 24:
+    christmas_year = christmas_year + 1
+
+# I assume christmas to be on the 24th
+christmas = datetime.date(christmas_year, 12, 24)
+
+print("There are still {} day(s) left until christmas.".format((christmas - date).days))
