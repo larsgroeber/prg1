fb424ae16a8bce4bdf5e29c43a0aa4e1b09d7af9
Lars Gröber <larsgroeber7@gmail.com>
2018-01-21 00:12:17 +0100

    added HuffmanTree class

---
+++ EPR/sheet7/huffman.py
@@ -0,0 +1,116 @@
+from collections import Counter
+from math import isclose
+
+
+class Node:
+    def __init__(self, weight: float):
+        self.weight = weight
+
+
+class Leaf(Node):
+    def __init__(self, value: chr, weight: float):
+        self.value = value
+        super().__init__(weight)
+
+    def __str__(self):
+        return f"{self.value}: {self.weight}"
+
+    def __eq__(self, other):
+        return self.value == other.value and isclose(self.weight, other.weight)
+
+    def __ne__(self, other):
+        return self.value != other.value or not isclose(self.weight, other.weight)
+
+
+class InternalNode(Node):
+    def __init__(self, weight: float, left: Node=None, right: Node=None):
+        self.left = left
+        self.right = right
+        super().__init__(weight)
+
+
+class HuffmanTree:
+    """Implements a huffman codec. Source: https://en.wikipedia.org/wiki/Huffman_coding"""
+
+    def __init__(self):
+        self.tree: InternalNode = None
+        self.code: dict = None
+        self.leafs: [Leaf] = None
+
+    def compress(self, text: str) -> str:
+        """
+            Compresses a text using the optimal Huffman code and returns
+            the encoded string.
+            The properties code, tree and leafs are set after running this method.
+            :param text:str: The text to compress
+        """
+        self.leafs = HuffmanTree.sort_leafs(HuffmanTree.create_leafs(text))
+        nodes: [Node] = list(self.leafs)
+        if not nodes:
+            return ""
+        if len(nodes) == 1:
+            nodes = [InternalNode(1, nodes[0])]
+        return self.__compress(text, nodes)
+
+    def __compress(self, text: str, nodes: [Node]) -> str:
+        while len(nodes) > 1:
+            leaf0 = nodes[0]
+            leaf1 = nodes[1]
+            internal_node = InternalNode(
+                leaf0.weight + leaf1.weight, leaf0, leaf1)
+            nodes = nodes[2:]
+            nodes.append(internal_node)
+        self.tree = nodes[0]
+        self.code = HuffmanTree.compute_code(self.tree)
+        return HuffmanTree.encode(text, self.code)
+
+    @staticmethod
+    def decode(text: str, code: dict) -> str:
+        """
+            Decodes a string using a given Huffman code.
+            :param text:str: The encoded string.
+            :param code:dict: The Huffman code: {"<chr>": "<code>"}
+        """
+        reversed_code = {y: x for x, y in code.items()}
+        result = ""
+        acc = ""
+        while text != "":
+            acc += text[0]
+            text = text[1:]
+            if acc in reversed_code:
+                result += reversed_code[acc]
+                acc = ""
+        if acc != "":
+            raise Exception(
+                f"Could not decode text, final accumulator: {acc}!")
+
+        return result
+
+    @staticmethod
+    def encode(text: str, code: dict) -> str:
+        result = ""
+        for letter in text:
+            if letter not in code:
+                raise Exception(f"Could not find code for {letter}!")
+            result += code[letter]
+        return result
+
+    @staticmethod
+    def compute_code(tree: InternalNode, largest_word=""):
+        code = dict()
+        for node, bit in [(tree.left, "0"), (tree.right, "1")]:
+            if isinstance(node, Leaf):
+                code[node.value] = largest_word + bit
+            elif node is not None:
+                code.update(HuffmanTree.compute_code(node, largest_word + bit))
+
+        return code
+
+    @staticmethod
+    def sort_leafs(input_leafs: [Leaf]) -> [Leaf]:
+        return sorted(input_leafs, key=lambda x: x.weight)
+
+    @staticmethod
+    def create_leafs(text: str) -> [Leaf]:
+        leafs = map(lambda t: Leaf(t[0], t[1]), Counter(text).items())
+        return map(lambda l: Leaf(l.value, l.weight / len(text)), leafs)
---
+++ EPR/sheet7/huffman_test.py
@@ -0,0 +1,89 @@
+import unittest
+
+from tree import HuffmanTree, Leaf, InternalNode
+
+
+class HuffmanTreeTest(unittest.TestCase):
+    def setUp(self):
+        self.huffman = HuffmanTree()
+        self.addTypeEqualityFunc(Leaf, self.do_leafs_equal)
+
+    def test_construct(self):
+        self.assertIsNotNone(self.huffman)
+
+    def test_leaf_sorting(self):
+        leafs = [
+            Leaf(1, 0.4),
+            Leaf(1, 0.1),
+            Leaf(1, 0.2),
+            Leaf(1, 0.1),
+            Leaf(1, 0.3),
+        ]
+        sorted_leafs = [
+            Leaf(1, 0.1),
+            Leaf(1, 0.1),
+            Leaf(1, 0.2),
+            Leaf(1, 0.3),
+            Leaf(1, 0.4),
+        ]
+        self.do_lists_of_leafs_equal(
+            sorted_leafs, HuffmanTree.sort_leafs(leafs))
+
+    def test_create_leafs(self):
+        leafs = HuffmanTree.create_leafs("test")
+        expect = [Leaf("t", 0.5), Leaf("e", 0.25), Leaf("s", 0.25)]
+        self.do_lists_of_leafs_equal(list(leafs), expect)
+
+        leafs = HuffmanTree.create_leafs("aaaaaaaAAAAbbbbb eee")
+        expect = [Leaf("a", 0.35), Leaf("A", 0.2), Leaf(
+            "b", 0.25), Leaf(" ", 0.05), Leaf("e", 0.15)]
+        self.do_lists_of_leafs_equal(list(leafs), expect)
+
+    def test_compute_code(self):
+        tree = InternalNode(2, Leaf("a", 0.5), Leaf("b", 0.5))
+        code = HuffmanTree.compute_code(tree)
+        self.assertEqual(code, {'a': '0', 'b': '1'})
+        tree = InternalNode(2, Leaf("a", 0.5),
+                            InternalNode(1, Leaf("b", 0.25), Leaf("c", 0.25)))
+        code = HuffmanTree.compute_code(tree)
+        self.assertEqual(code, {'a': '0', 'b': '10', 'c': '11'})
+        tree = InternalNode(2, Leaf("a", 0.5),
+                            InternalNode(1, Leaf("b", 0.25),
+                                         InternalNode(1, Leaf("c", 0.1), Leaf("d", 0.15))))
+        code = HuffmanTree.compute_code(tree)
+        self.assertEqual(code, {'a': '0', 'b': '10', 'c': '110', 'd': '111'})
+
+    def test_encode(self):
+        code = {'a': '0', 'b': '1'}
+        self.assertEqual(HuffmanTree.encode("ababbab", code), "0101101")
+        self.assertEqual(HuffmanTree.encode("", code), "")
+        code = {'a': '0', 'b': '1'}
+        self.assertRaises(Exception, HuffmanTree.encode, "abcabbab", code)
+
+    def test_compress(self):
+        self.assertEqual(self.huffman.compress("test"), "010110")
+        self.assertEqual(self.huffman.compress("AAAABBCC"), "000010101111")
+        self.assertEqual(self.huffman.compress("AAAA"), "0000")
+        self.assertEqual(self.huffman.compress("AAAA\nAAA"), "11110111")
+
+    def test_decode(self):
+        self.assertEqual(HuffmanTree.decode(
+            "010110", {"t": "0", "e": "10", "s": "11"}), "test")
+        self.assertEqual(HuffmanTree.decode(
+            "00101111100", {"t": "00", "e": "1011", "s": "111"}), "test")
+        self.assertRaises(Exception, HuffmanTree.decode,
+                          "0101101", {"t": "0", "e": "10", "s": "11"})
+
+    def do_leafs_equal(self, a: Leaf, b: Leaf, msg=None):
+        if a != b:
+            raise self.failureException(f"{a} does not match {b}!")
+
+    def do_lists_of_leafs_equal(self, a: [Leaf], b: [Leaf], msg=None):
+        for i, leaf in enumerate(a):
+            if i >= len(b):
+                self.failureException("Lists of leafs do not match!")
+            self.assertEqual(leaf, b[i])
+
+
+if __name__ == "__main__":
+    unittest.main()
