af0d95b88532e6acec46c8fedee0b2ba266effe0
Lars Gröber <larsgroeber7@gmail.com>
2018-01-21 13:04:06 +0100

    added documentation

--- EPR/sheet7/huffman.py
+++ EPR/sheet7/huffman.py
@@ -1,5 +1,6 @@
 from collections import Counter
 from math import isclose
+from typing import Dict, List
 
 
 class Node:
@@ -29,12 +30,15 @@ class InternalNode(Node):
         super().__init__(weight)
 
 
+CODETYPE = {chr, str}
+
+
 class HuffmanTree:
     """Implements a huffman codec. Source: https://en.wikipedia.org/wiki/Huffman_coding"""
 
     def __init__(self):
         self.tree: InternalNode = None
-        self.code: dict = None
+        self.code: CODETYPE = None
         self.leafs: [Leaf] = None
 
     def compress(self, text: str) -> str:
@@ -52,6 +56,23 @@ class HuffmanTree:
             nodes = [InternalNode(1, nodes[0])]
         return self.__compress(text, nodes)
 
+    @staticmethod
+    def sort_leafs(input_leafs: [Leaf]) -> [Leaf]:
+        """
+            Returns input_leafs sorted by weight (lowest first).
+            :param input_leafs:[Leaf]: Leafs to sort.
+        """
+        return sorted(input_leafs, key=lambda x: x.weight)
+
+    @staticmethod
+    def create_leafs(text: str) -> [Leaf]:
+        """
+            Returns leafs for a given text.
+            :param text:str: Text to generate leafs from.
+        """
+        return map(lambda t: Leaf(
+            t[0], t[1] / len(text)), Counter(text).items())
+
     def __compress(self, text: str, nodes: [Node]) -> str:
         while len(nodes) > 1:
             leaf0 = nodes[0]
@@ -65,7 +86,37 @@ class HuffmanTree:
         return HuffmanTree.encode(text, self.code)
 
     @staticmethod
-    def decode(text: str, code: dict) -> str:
+    def compute_code(tree: InternalNode, largest_word="") -> CODETYPE:
+        """
+            Iteratively computes the code from a Huffman tree.
+            :param tree:InternalNode: Current root node of the Huffman tree
+            :param largest_word="": Code for the root node.
+        """
+        code = dict()
+        for node, bit in [(tree.left, "0"), (tree.right, "1")]:
+            if isinstance(node, Leaf):
+                code[node.value] = largest_word + bit
+            elif node is not None:
+                code.update(HuffmanTree.compute_code(node, largest_word + bit))
+
+        return code
+
+    @staticmethod
+    def encode(text: str, code: CODETYPE) -> str:
+        """
+            Encodes a given string using a given Huffman code.
+            :param text:str: The text to encode.
+            :param code:dict: The code to use.
+        """
+        result = ""
+        for letter in text:
+            if letter not in code:
+                raise Exception(f"Could not find code for {letter}!")
+            result += code[letter]
+        return result
+
+    @staticmethod
+    def decode(text: str, code: CODETYPE) -> str:
         """
             Decodes a string using a given Huffman code.
             :param text:str: The encoded string.
@@ -85,32 +136,3 @@ class HuffmanTree:
                 f"Could not decode text, final accumulator: {acc}!")
 
         return result
-
-    @staticmethod
-    def encode(text: str, code: dict) -> str:
-        result = ""
-        for letter in text:
-            if letter not in code:
-                raise Exception(f"Could not find code for {letter}!")
-            result += code[letter]
-        return result
-
-    @staticmethod
-    def compute_code(tree: InternalNode, largest_word=""):
-        code = dict()
-        for node, bit in [(tree.left, "0"), (tree.right, "1")]:
-            if isinstance(node, Leaf):
-                code[node.value] = largest_word + bit
-            elif node is not None:
-                code.update(HuffmanTree.compute_code(node, largest_word + bit))
-
-        return code
-
-    @staticmethod
-    def sort_leafs(input_leafs: [Leaf]) -> [Leaf]:
-        return sorted(input_leafs, key=lambda x: x.weight)
-
-    @staticmethod
-    def create_leafs(text: str) -> [Leaf]:
-        leafs = map(lambda t: Leaf(t[0], t[1]), Counter(text).items())
-        return map(lambda l: Leaf(l.value, l.weight / len(text)), leafs)
--- EPR/sheet7/huffman_test.py
+++ EPR/sheet7/huffman_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from tree import HuffmanTree, Leaf, InternalNode
+from huffman import HuffmanTree, Leaf, InternalNode
 
 
 class HuffmanTreeTest(unittest.TestCase):
